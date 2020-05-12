5ad7f47eb5b252b1cd366e556f28ef2b5cd38548
ZakariaZanati <zanati.zakaria1@gmail.com>
2020-05-12 01:35:55 +0000

    Tests unitaires

---
+++ Block Breaker/Assets/Editor/saving_test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+
+namespace Tests
+{
+public class saving_test 
+{
+    [Test]
+        public void TestUnitaire_SavingLevelSimplePasses()
+        {
+            var sceneLoader = new SceneLoader();
+            int ActualLevl = SceneLoader.ActualLevel();
+            Assert.AreEqual(1,ActualLevl);
+        }
+
+        [Test]
+        public void TestUnitaire_SavingScoreSimplePasses()
+        {
+            var sceneLoader = new SceneLoader();
+            int Actualscore = SceneLoader.ActualScore();
+            Assert.AreEqual(144,Actualscore);
+        }
+}
+}
